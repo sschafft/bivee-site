6502ce19a098aa9dbebc632fdf227dd6e46f2b7d
Nicholas Kingston <nickingston@gmail.com>
2013-06-14 10:25:56 -0400

    Cleaned up styles
    Added some additional cosmetic features and cleaned up LESS files.

--- assets/javascripts/bvload.js
+++ assets/javascripts/bvload.js
@@ -1,5 +1,6 @@
 $(window).load(function() {
 		// Animate loader off screen
+		$("#glare").fadeIn(500);
 		$("#loading").fadeOut(1000, function(){
 			$("#logo").fadeIn(600, function(){
 				$("#container").fadeIn(700, function(){
