b449f1298b248cd51ff887d14c01c5ef72542e31
Jay Perry <j@jayperryworks.com>
2016-09-01 16:33:26 -0400

    updated js

---
+++ .tmp/dist/bundle.js
@@ -0,0 +1,13 @@
+webpackJsonp([0],[
+/* 0 */
+/*!*******************************************!*\
+  !*** ./source/assets/javascripts/main.js ***!
+  \*******************************************/
+/***/ function(module, exports) {
+
+	//= require jquery
+	"use strict";
+
+/***/ }
+]);
+//# sourceMappingURL=bundle.js.map
