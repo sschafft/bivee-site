9105107441b14ce211fa6edc8d8ae5efb518ff79
Jay Perry <j@jayperryworks.com>
2016-09-01 16:46:59 -0400

    updated webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         ],
     },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin("critical", "critical.bundle.js"),
+        new webpack.optimize.CommonsChunkPlugin("critical", "bundle.critical.js"),
         new Clean(['.tmp']),
     ]
 };
