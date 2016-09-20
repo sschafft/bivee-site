62b9ef5c61339407fe79c1309d47efbc043f1153
Jay Perry <j@jayperryworks.com>
2016-09-20 17:42:58 -0400

    setting up tooltips

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,10 +1,12 @@
 // import $ from 'jquery';
 import Clipboard from 'clipboard';
 
+// Copy-to-clipboard script
+// -----------------------------------------------------------------------------
 const emailButton = document.getElementById('email_to_clipboard');
 const clipboard = new Clipboard(emailButton);
 
-clipboard.on('success', function(e) {
+clipboard.on('success', e => {
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
@@ -12,7 +14,7 @@ clipboard.on('success', function(e) {
     e.clearSelection();
 });
 
-clipboard.on('error', function(e) {
+clipboard.on('error', e => {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
 });
