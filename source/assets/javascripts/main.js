b34c317a643bab4f511a83baa8a28d41c1c6505a
Jay Perry <j@jayperryworks.com>
2016-09-20 12:13:02 -0400

    setting up copy-to-clipboard for email

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,3 +1,12 @@
 // import $ from 'jquery';
 import clipboard from 'clipboard';
 
+clipboard = new Clipboard('#email_to_clipboard');
+
+clipboard.on('success', e => {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
