c2ba7249519818398e755e2aee8b44bc75450c58
Jay Perry <j@jayperryworks.com>
2016-09-20 16:11:27 -0400

    copy-to-clipboard JS working

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,12 +1,20 @@
 // import $ from 'jquery';
-import clipboard from 'clipboard';
+import Clipboard from 'clipboard';
 
-clipboard = new Clipboard('#email_to_clipboard');
+const emailButton = document.getElementById('email_to_clipboard');
+const clipboard = new Clipboard(emailButton);
 
-clipboard.on('success', e => {
+clipboard.on('success', function(e) {
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
 
     e.clearSelection();
 });
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
+
+sayhi@bivee.co
