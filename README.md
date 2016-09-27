60f72bef6191820eef5a256988915779a206b216
Scott Schaffter <scott@bivee.co>
2016-09-26 17:49:10 -0700

    adjusted deployment method

--- README.md
+++ README.md
@@ -11,3 +11,10 @@
 
 - Run `bundle exec middleman` to start a local Middleman server; site will be available at `http://localhost:4567` in your browser. You can see the sitemap and config at `http://localhost:4567/__middleman`.
 - `bundle exec middleman build` will deploy the site to the `/build` folder.
+
+## To Deploy:
+
+- Add remote to main bivee site: `git remote add deploy-repo git@github.com:biveeco/biveeco.github.io.git`
+- https://github.com/biveeco/biveeco.github.io
+- Run `middleman build`
+- Run `middleman deploy`
--- config.rb
+++ config.rb
@@ -30,10 +30,10 @@ set :markdown_engine, :kramdown
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  deploy.remote   = 'deploy-repo' # remote name or git url, default: origin
   deploy.branch   = 'master' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   deploy.commit_message = 'Deployed to master'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
 end
 
 
