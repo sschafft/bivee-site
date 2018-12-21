379a51d5144b78849271eaa0adb3d782deb3cefb
Jay Perry <j@jayperryworks.com>
2018-12-21 14:03:04 -0500

    [#75] refine behaviors for flyout

--- source/assets/javascripts/modules/flyout.js
+++ source/assets/javascripts/modules/flyout.js
@@ -1,11 +1,13 @@
-export default function flyout (attribute, toggleAttribute = 'active') {
+// Flyout module: queries an element by data attribute and toggles a class on it
+// Params:
+// attribute: (string, required) the name of the data attribute of the trigger element (e.g. a button with 'data-js-toggle')
+// toggleAttribute: (string, optional) the 'active' class name for both the trigger and the target element
+export default function flyout (attribute, toggleAttribute = 'is-active') {
   const triggers = document.querySelectorAll(`[${attribute}]`)
-  console.log(triggers)
 
   triggers.forEach(trigger => {
     const targetName = trigger.getAttribute(attribute)
     const target = document.getElementById(targetName)
-    console.log(targetName)
 
     if (!target) {
       return
